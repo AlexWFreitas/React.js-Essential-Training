aa0404e2c0d77320213788d8405ef16566651005
Alex Wong <alex.wong@orangetesting.com>
2021-11-07 14:52:55 -0300

    Chapter 4 - Conditional Rendering and Destructuring Arrays and Objects

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -1,64 +1,22 @@
 import './App.css';
-import restaurant from "./restaurant.png";
 
-function Header(props) {
-  return (
-    <header>
-      <h1>{props.name}'s Kitchen</h1>
-    </header>
-  );
+function SecretComponent() {
+  return <h1>Secret information for authorized users only</h1>;
 }
 
-function Main(props) {
-  return (
-    <section>
-      <p>We serve the most {props.adjective} food around.</p>
-      <img 
-        src={restaurant} 
-        height={200} 
-        alt="Restaurant Logo" 
-      />
-      <ul style = {{textAlign: "left"}}>
-        {props.dishes.map((dish) => (
-          <li key={dish.id}>{dish.title}</li>
-        ))}
-      </ul>      
-    </section>
-  );
+function RegularComponent() {
+  return <h1>Everyone can see this component.</h1>;
 }
 
-function Footer(props) {
+function App( {authorized} ) {  
   return (
-    <footer>
-      <p>Copyright {props.year}</p>    
-    </footer>
-  );
-}
-
-const dishes = [
-  "Macaroni and Cheese",
-  "Salmon",
-  "Tofu with Vegetables",
-  "Minestrone"
-];
-
-const dishObjects = dishes.map((dish, i) => (
-  {
-    id: i, 
-    title: dish
-  })
-);
-
-// console.log(dishObjects);
-
-function App() {
-  return (
-    <div className="App">
-      <Header name="Alex" />
-      <Main adjective="amazing" dishes={dishObjects}/> 
-      <Footer year={new Date().getFullYear()}/> 
-    </div>
-  );
+    <>
+      {authorized
+        ? <SecretComponent />
+        : <RegularComponent />
+      }
+    </>
+  )
 }
 
 export default App;
--- react-essentials/src/index.js
+++ react-essentials/src/index.js
@@ -3,7 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App'
 
+// Array Destructuring Example
+const [mostImportantItem, secondMostImportantItem, thirdMostImportantItem] = ["boots", "tent", "headlamp"];
+
+console.log(mostImportantItem); // boots
+console.log(secondMostImportantItem); // tent
+console.log(thirdMostImportantItem); // headlamp
+
+const [, , light] = ["boots", "tent", "headlamp"];
+
+console.log(light); // headlamp
+
 ReactDOM.render(
-	<App />, 
+	<App authorized={false}/>, 
 	document.getElementById('root')
 );
