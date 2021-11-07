8b2327a04afb80313bcbe6d8be53209926d7b07b
Alex Wong de Freitas <otaru1921@gmail.com>
2021-11-07 15:30:59 -0300

    React Hooks - useEffect and useState

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -1,20 +1,26 @@
+import React, { useState, useEffect } from "react";
 import './App.css';
 
-function SecretComponent() {
-  return <h1>Secret information for authorized users only</h1>;
-}
+function App() {  
+  const [emotion, setEmotion] = useState("happy");
 
-function RegularComponent() {
-  return <h1>Everyone can see this component.</h1>;
-}
+  const [secondary, setSecondary] = useState("tired");  
 
-function App( {authorized} ) {  
+  useEffect(() => {
+    console.log(`It's ${emotion} around here!`);
+  }, [emotion]);
+
+  useEffect(() => {
+    console.log(`It's ${secondary} around here!`);
+  }, [secondary]);
+    
   return (
     <>
-      {authorized
-        ? <SecretComponent />
-        : <RegularComponent />
-      }
+      <h1>Current emotion is {emotion} and {secondary}.</h1>
+      <button onClick={() => setEmotion("happy")}>Make Happy</button>
+      <button onClick={() => setSecondary("crabby")}>Make Crabby</button>
+      <button onClick={() => setEmotion("frustrated")}>Frustrate</button>
+      <button onClick={() => setEmotion("enthusiastic")}>Enthuse</button>
     </>
   )
 }
--- react-essentials/src/index.js
+++ react-essentials/src/index.js
@@ -3,18 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App'
 
-// Array Destructuring Example
-const [mostImportantItem, secondMostImportantItem, thirdMostImportantItem] = ["boots", "tent", "headlamp"];
-
-console.log(mostImportantItem); // boots
-console.log(secondMostImportantItem); // tent
-console.log(thirdMostImportantItem); // headlamp
-
-const [, , light] = ["boots", "tent", "headlamp"];
-
-console.log(light); // headlamp
-
 ReactDOM.render(
-	<App authorized={false}/>, 
+	<App />, 
 	document.getElementById('root')
 );
